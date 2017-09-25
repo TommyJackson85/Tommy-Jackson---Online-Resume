ad8546270da856ba864bc20c6e9eee2b1f97e11e
ThomasJ85 <tom.jackson50@gmail.com>
2017-09-25 15:14:51 +0800

    Removed files and code, changed colors
    Removed unnecessary files and code., i.e. empty media queries, @import
    files.

--- js/helper.js
+++ js/helper.js
@@ -1,49 +1,31 @@
-/*
 
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
- 
 /*1-About Section variables*/
 var HTMLbioPic = '<img src=%data% alt="..." id="profilePic" class="col-xs-6" />';
 var HTMLheaderName =  '<h1>%data%</h1>';
 var HTMLbirth = '<h2><i>D.O.B: %data% </i></h2>';
 var HTMLcurrentLocation = '<h2><i>Location: %data% </i></h2>';
 var HTMLabout = '<p>%data%</p>';
-/*END of About Section variables*/
 
 /*2-Contact variables for EACH contact*/
 var HTMLcontact = '<div class="contact">%data%</div>';
-var HTMLlink = '<div class="page"><a href="%data%" class="link"></a></div>';
+var HTMLlink = '<div class="pageIcon"><a href="%data%" class="link"></a></div>';
 var HTMLpageType = '<h4>%data%</h4>';
-//var HTMLpageData = '<p>%data%</p>';
-/*END of Contact variables*/
 
 /*3-Progress Bar variables*/
 /*--Combine the Three Variables to create each Progress Bar*/
 var HTMLbarChartData = '<div class="progress col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12"><div class="progress-bar" role="progressbar" aria-valuenow="%data%" aria-valuemin="0" '; 
 var HTMLbarChartPercentage = 'aria-valuemax="100" style="width: %data%;">';
 var HTMLbarChartText = '%data%</div></div>';
-/*END of Progress Bar variables*/
 
-/*4-variables for BOTH Education and Projects.*/
+/*4-variables for BOTH Education and Projects.*/ /*MAYBE DELETE AND MAKE MORE EFFICIENT*/
 /*--Starting Variables*/
-var HTMLonlineEducation = '<div id="onlineEdu"><h5>Online Education</h5><ul id="onlineList"></ul></div>';
-var HTMLonlineListItem = '<li class="onlineListItem">%data%</li>';
+//var HTMLonlineEducation = '<div id="onlineEdu"><h6>Online Education</h6><ul id="onlineList"></ul></div>';
+var HTMLonlineListItem = '<li class="onlineListItem"><i>%data%</i></li>';
 
-var HTMLotherEducation = '<div id="otherEdu"><h5>Other Education</h5><ul id="otherList"></ul></div>';
-var HTMLotherListItem = '<li class="otherListItem">%data%</li>';
+//var HTMLotherEducation = '<div id="otherEdu"><h6>Other Education</h6><ul id="otherList"></ul></div>';
+var HTMLotherListItem = '<li class="otherListItem"><i>%data%</i></li>';
 
-var HTMLprojects = '<div id="projects"><h5>Projects</h5><ul id="projectsList"></ul></div>';
+//var HTMLprojects = '<div id="projects"><h5>Projects</h5><ul id="projectsList"></ul></div>';
 var HTMLprojectsListItem = '<li class="projectListItem"><h6>%data%</h6>';
 
 /*--Ending Variables*/
@@ -51,10 +33,9 @@ var HTMLdescription = '<p>%data%</p></li>';
 var HTMLlistItemTitleDescription = '<li class="listItem"><h6>item</h6><p>description</p></li>';
 var HTMLitemTitle = '<li class="listItem"><h6>title</h6></li>';
 var HTMLendItem = '</li>';
-/*END of variables for BOTH Education and Experience.*/
 
 /*5-Work Experience HTML*/
-var HTMLwork = '<div id="work"><h5>Work Experience</h5><ul id="workList"></ul>';
+//var HTMLwork = '<div id="work"><h5>Work Experience</h5><ul id="workList"></ul>';
 var HTMLjobRole = '<li class="jobListItem"><h6>%data%</h6></li>';
 var HTMLemployerDates = '<h6><i>%data%</i></h6>';
 /*END of Work Experience variables.*/
--- js/resumeBuilder2.js
+++ js/resumeBuilder2.js
@@ -5,8 +5,6 @@ var bio = {
 "location" : "Perth, Australia",
 "about" : `I am a <strong>Frontend Developer</strong> who practices JavaScript everyday. I'm also learning Angular 4, Node JS and the Mean stack and I am enrolled in Free Code Camp's full stack course. Much of my past work experience and education has allowed me to inherit skills in project management, team-work and communications. My hobbies and interests include cinema, reading, photography, screenwriting, swimming and cycling.`,
 } ;
-
-
 var formattedPic = HTMLbioPic.replace("%data%", bio.biopic);
 $("#profileDetails").append(formattedPic);
 console.log(formattedPic);
@@ -27,7 +25,7 @@ var formattedAbout = HTMLabout.replace("%data%", bio.about);
 $("#profileDetails").append(formattedAbout);
 
 
-var contacts = {
+	var contacts = {
 	"phone" : "<h4><i class='fa fa-phone' aria-hidden='true'></i>	<i>xxx xxx xxxx</i></h4>",
 	"email" : "<h4><i class='fa fa-envelope' aria-hidden='true'></i>	<i>tom.jackson50@gmail.com<i/></h4>",
 	"internetPage" : [
@@ -37,179 +35,163 @@ var contacts = {
 		"link" : "https://plus.google.com/u/0/110016559790020821468"},
 		{"type" : "<i class='fa fa-twitter-square' aria-hidden='true'></i>",
 		"link" : ""},
-				]};
+	]};
 
-			function displayContacts () {
-var formattedPhone = HTMLcontact.replace('%data%', contacts.phone);
-var formattedEmail = HTMLcontact.replace('%data%', contacts.email);
-$("#contact").append(formattedPhone);
-$("#contact").append(formattedEmail);
+	function displayContacts () {
+	var formattedPhone = HTMLcontact.replace('%data%', contacts.phone);
+	var formattedEmail = HTMLcontact.replace('%data%', contacts.email);
+	$("#contact").append(formattedPhone);
+	$("#contact").append(formattedEmail);
 
-for (Page in contacts.internetPage) { 
+	for (Page in contacts.internetPage) { 
 
-var formattedLink = HTMLlink.replace("%data%", contacts.internetPage[Page].link);
+	var formattedLink = HTMLlink.replace("%data%", contacts.internetPage[Page].link);
 
-$("#contact").append(formattedLink);
-console.log(HTMLlink);
+	$("#contact").append(formattedLink);
+	console.log(HTMLlink);
 
-var formattedPageType = HTMLpageType.replace("%data%", contacts.internetPage[Page].type);
-$(".link:last").append(formattedPageType);
-console.log(formattedPageType);
-
-/*var formattedContactData = HTMLcontactData.replace("%data%", contacts.internetProfiles[Contact].data);
-$(".contact:last").append(formattedContactData);
-console.log(formattedContactData);*/
-
-}
-}
-displayContacts ()
-
-var progressChart = {
-	"skills": [{
-		"text": "Javascript (85%)",
-		"data": 85,
-		"percentage": "85%"},
-		{
-		"text": "Angular 4 (20%)",
-		"data": 20,
-		"percentage": "20%"},
-		{
-		"text": "Node JS (20%)",
-		"data": 20,
-		"percentage": "20%"},
-		{
-		"text": "JQuery (70%)",
-		"data": 70,
-		"percentage": "70%"},
-		{
-		"text": "SASS / SCSS (50%)",
-		"data": 50,
-		"percentage": "50%"},
-		{
-		"text": "Bootstrap (100%)",
-		"data": 100,
-		"percentage": "100%"},
-		{
-		"text": "HTML5 (100%)",
-		"data": 100,
-		"percentage": "100%"},
-		{
-		"text": "CSS3 (100%)",
-		"data": 100,
-		"percentage": "100%"},
-		]
+	var formattedPageType = HTMLpageType.replace("%data%", contacts.internetPage[Page].type);
+	$(".link:last").append(formattedPageType);
+	console.log(formattedPageType);
 	}
+	}
+	displayContacts ()
 
-function displayChart () {
+			var progressChart = {
+			"skills": [{
+				"text": "Javascript (85%)",
+				"data": 85,
+				"percentage": "85%"},
+				{
+				"text": "Angular 4 (30%)",
+				"data": 30,
+				"percentage": "30%"},
+				{
+				"text": "Node JS (30%)",
+				"data": 30,
+				"percentage": "30%"},
+				{
+				"text": "JQuery (70%)",
+				"data": 70,
+				"percentage": "70%"},
+				{
+				"text": "SASS / SCSS (50%)",
+				"data": 50,
+				"percentage": "50%"},
+				{
+				"text": "Bootstrap (100%)",
+				"data": 100,
+				"percentage": "100%"},
+				{
+				"text": "HTML5 (100%)",
+				"data": 100,
+				"percentage": "100%"},
+				{
+				"text": "CSS3 (100%)",
+				"data": 100,
+				"percentage": "100%"},
+			]
+			}
 
-for (skill in progressChart.skills) {
+			function displayChart () {
 
-var formattedBarChartData = HTMLbarChartData.replace("%data%", progressChart.skills[skill].data) ;
-console.log(formattedBarChartData);
+			for (skill in progressChart.skills) {
 
-var formattedBarChartPercentage = HTMLbarChartPercentage.replace("%data%", progressChart.skills[skill].percentage) ; 
-console.log(formattedBarChartPercentage);
+			var formattedBarChartData = HTMLbarChartData.replace("%data%", progressChart.skills[skill].data) ;
+			console.log(formattedBarChartData);
 
-var formattedBarChartText = HTMLbarChartText.replace("%data%", progressChart.skills[skill].text);
-console.log(formattedBarChartText);
+			var formattedBarChartPercentage = HTMLbarChartPercentage.replace("%data%", progressChart.skills[skill].percentage) ; 
+			console.log(formattedBarChartPercentage);
 
-var formattedBarChartFull = formattedBarChartData + formattedBarChartPercentage + formattedBarChartText;
+			var formattedBarChartText = HTMLbarChartText.replace("%data%", progressChart.skills[skill].text);
+			console.log(formattedBarChartText);
 
-$("#barChart").append(formattedBarChartFull);	
-console.log(formattedBarChartFull);
-} }
+			var formattedBarChartFull = formattedBarChartData + formattedBarChartPercentage + formattedBarChartText;
 
-displayChart();
+			$("#barChart").append(formattedBarChartFull);	
+			console.log(formattedBarChartFull);
+			} }
+
+			displayChart();
 
 var education = {
-	"online": [
-		{"company": "Udemy",
-		"detail": "Currently enrolled in 'Angula 2 and 4', 'Node JS' and 'The MEAN Stack' tutorials "
-		},
-		{
-		"company": "Free Code Camp",
-		"detail": "Currently Enroled in "},
-		{
-		"company": "Team Tree House",
-		"detail": "Covered JavaScript Basics and Bootstrap video tutorials."},
-		],
+"online": [
+	{"company": "Udemy",
+	"detail": "Currently enrolled in 'Angula 2 and 4', 'Node JS' and 'The MEAN Stack' tutorials."
+	},
+	{"company": "Free Code Camp",
+	"detail": "Currently Enroled in FCC's Front End Development Certification course."},
+	{"company": "Team Tree House",
+	"detail": "Covered JavaScript Basics and Bootstrap video tutorials."},
+],
 
-	"other": [
-		{
-		"company": "University College Cork, Ireland",
-		"detail": "Received a Second Class Honours, Grade 1 BA Degree. Majored in Geography and Study of Religions. Worked with GIS technology such as ArcGIS for my disertation and other projects"},
-		{
-		"company": "European Computer Drivers License Certificate (2013)",
-		"detail": ""}
-		]
-	}
+"other": [
+	{"company": "University College Cork, Ireland",
+	"detail": "Received a Second Class Honours, Grade 1 BA Degree. Majored in Geography and Study of Religions. Worked with GIS technology such as ArcGIS for my disertation and other projects."},
+	{"company": "European Computer Drivers License Certificate (2013)",
+	"detail": ""}
+]
+}
 
 function displayEducation () {
 
-	$("#educationList").append(HTMLonlineEducation);
-	console.log(HTMLonlineEducation);
+//$("#educationList").append(HTMLonlineEducation);
+//console.log(HTMLonlineEducation);
 	
-	for (Online in education.online) {
-	var formattedOnlineListItem = HTMLonlineListItem.replace("%data%", education.online[Online].company);
-	var	formattedOnlineDescription = HTMLdescription.replace("%data%", education.online[Online].detail);
-	var formatteOnlineListEntry = formattedOnlineListItem + formattedOnlineDescription;
-	$("#onlineList:last").append(formatteOnlineListEntry);
-	}
-
-$("#educationList").append(HTMLotherEducation);
-	console.log(HTMLotherEducation);
-	
-	for (Other in education.other) {
-	var formattedOtherListItem = HTMLotherListItem.replace("%data%", education.other[Other].company);
-	var	formattedOtherDescription = HTMLdescription.replace("%data%", education.other[Other].detail);
-	var formatteOtherListEntry = formattedOtherListItem + formattedOtherDescription;
-	$("#otherList:last").append(formatteOtherListEntry);
-	}
-
+for (Online in education.online) {
+var formattedOnlineListItem = HTMLonlineListItem.replace("%data%", education.online[Online].company);
+var	formattedOnlineDescription = HTMLdescription.replace("%data%", education.online[Online].detail);
+var formatteOnlineListEntry = formattedOnlineListItem + formattedOnlineDescription;
+$("#onlineList:last").append(formatteOnlineListEntry);
 }
-	displayEducation ()
-	 var experience = {
-	"projects": [
-		{"project" : "<a href='https://github.com/TommyJackson85'><h6><i class='fa fa-github' aria-hidden='true'></i> Github page</h6></a>",
-		"detail" : "Collection of projects."},
-		{
-		"project": "<a href='https://codepen.io/ThomasJ85/collections/popular/'><h6><i class='fa fa-codepen' aria-hidden='true'></i>  Codepen page</h6></a>",
-		"detail": "Collection of projects and examples of code."}
-		],
-	"work": [
-		{
-		"role": "Quality Control - General Operative.",
-		"employerDates": "Apple European Headquarters - September 2015 to January 2016 November 2016 to Present."},
-		{
-		"role": "Production Assistant.",
-		"employerDates": "EPIC Productions, Cork Ireland - January 2015 to September 2016."},
-		{
-		"role": "General Operative.",
-		"employerDates": "CPL Recruitment - August 2015 to September 2016, August 2016 to October 2016."}
-		],
-	}
-
-function displayExperience () {
-
-	$("#experienceList").append(HTMLprojects);
-	console.log(HTMLprojects);
+//$("#educationList").append(HTMLotherEducation);
+//console.log(HTMLotherEducation);
 	
-	for (Project in experience.projects) {
-	var formattedProjectsListItem = HTMLprojectsListItem.replace("%data%", experience.projects[Project].project);
-	var	formattedProjectsDescription = HTMLdescription.replace("%data%", experience.projects[Project].detail);
-	var formattedProjectsListEntry = formattedProjectsListItem + formattedProjectsDescription;
-	$("#projectsList").append(formattedProjectsListEntry);
-	}
-
-$("#experienceList").append(HTMLwork);
-	console.log(HTMLwork);
-	
-	for (Work in experience.work) {
-	var formattedJobRole = HTMLjobRole.replace("%data%", experience.work[Work].role);
-	var	formattedEmployerDates = HTMLemployerDates.replace("%data%", experience.work[Work].employerDates);
-	var formattedWorkListEntry = formattedJobRole + formattedEmployerDates;
-	$("#workList:last").append(formattedWorkListEntry);
-	}
-
+for (Other in education.other) {
+var formattedOtherListItem = HTMLotherListItem.replace("%data%", education.other[Other].company);
+var	formattedOtherDescription = HTMLdescription.replace("%data%", education.other[Other].detail);
+var formatteOtherListEntry = formattedOtherListItem + formattedOtherDescription;
+$("#otherList:last").append(formatteOtherListEntry);
 }
-displayExperience ()
+}
+displayEducation ()
+	 
+		var experience = {
+		"projects": [
+			{"project" : "<a href='https://github.com/TommyJackson85'><h6><i class='fa fa-github' aria-hidden='true'></i> Github page</h6></a>",
+			"detail" : "Collection of projects."},
+			{
+			"project": "<a href='https://codepen.io/ThomasJ85/collections/popular/'><h6><i class='fa fa-codepen' aria-hidden='true'></i>  Codepen page</h6></a>",
+			"detail": "Collection of projects and examples of code."}
+		],
+		"work": [
+			{"role": "Quality Control - General Operative.",
+			"employerDates": "Apple European Headquarters - September 2015 to January 2016 November 2016 to Present."},
+			{"role": "Production Assistant.",
+			"employerDates": "EPIC Productions, Cork Ireland - January 2015 to September 2016."},
+			{"role": "General Operative.",
+			"employerDates": "CPL Recruitment - August 2015 to September 2016, August 2016 to October 2016."}
+		],
+		}
+
+		function displayExperience () {
+		//$("#experienceList").append(HTMLprojects);
+		//console.log(HTMLprojects);
+		for (Project in experience.projects) {
+		var formattedProjectsListItem = HTMLprojectsListItem.replace("%data%", experience.projects[Project].project);
+		var	formattedProjectsDescription = HTMLdescription.replace("%data%", experience.projects[Project].detail);
+		var formattedProjectsListEntry = formattedProjectsListItem + formattedProjectsDescription;
+		$("#projectsList").append(formattedProjectsListEntry);
+		}
+
+		//$("#experienceList").append(HTMLwork);
+		//console.log(HTMLwork);
+	
+		for (Work in experience.work) {
+		var formattedJobRole = HTMLjobRole.replace("%data%", experience.work[Work].role);
+		var	formattedEmployerDates = HTMLemployerDates.replace("%data%", experience.work[Work].employerDates);
+		var formattedWorkListEntry = formattedJobRole + formattedEmployerDates;
+		$("#workList:last").append(formattedWorkListEntry);
+		}
+		}
+		displayExperience ()
